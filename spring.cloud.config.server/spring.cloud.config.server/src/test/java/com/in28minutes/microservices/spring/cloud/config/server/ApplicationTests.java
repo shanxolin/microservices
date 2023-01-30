18544fe2e7b6444e2710cefa56b6ea1ff225005e
sancho garcia-tenorio rebanal <sancho.garcia.tenorio@gmail.com>
2023-01-30 17:19:33 +0100

    upload files

---
+++ spring.cloud.config.server/spring.cloud.config.server/src/test/java/com/in28minutes/microservices/spring/cloud/config/server/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.spring.cloud.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
